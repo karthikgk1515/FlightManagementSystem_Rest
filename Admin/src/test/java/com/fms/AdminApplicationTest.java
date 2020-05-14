d6c36e11bd5a389385d7f6aa65d9d725d357a927
karthikgk1515 <karthikganji1515@gmail.com>
2020-05-14 15:02:04 +0530

    first commit

--- Admin/src/test/java/com/fms/AdminApplicationTest.java
+++ Admin/src/test/java/com/fms/AdminApplicationTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -18,12 +19,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.fms.dto.Userdata;
 import com.fms.repository.AirportRepository;
 import com.fms.repository.FlightRepository;
+import com.fms.repository.ScheduledFlightRepository;
 import com.fms.repository.UserRepository;
 import com.fms.dto.Airport;
 import com.fms.service.FlightService;
 import com.fms.service.ScheduledflightService;
 import com.fms.service.UserService;
 import com.fms.dto.Flight;
+import com.fms.dto.Scheduledflight;
 
 @SpringBootTest
 class AdminApplicationTest {
@@ -33,6 +36,22 @@ class AdminApplicationTest {
 	@InjectMocks
 	private UserService userService;
 	
+
+	@Mock
+	private ScheduledFlightRepository sfdao;
+	
+	@InjectMocks
+	private ScheduledflightService sfService;
+	
+
+	@Mock
+	private FlightRepository fdao;
+	
+	@InjectMocks
+	private FlightService flightService;
+	
+	@Mock
+	private AirportRepository adao;
 	
 	@Test
 	public void testfindAlluser() {
@@ -64,12 +83,6 @@ class AdminApplicationTest {
 		assertEquals(true,result);      		
 }
 
-
-	@Mock
-	private FlightRepository fdao;
-	
-	@InjectMocks
-	private FlightService flightService;
 	
 	@Test
 	public void testfindAllflights() {
@@ -107,16 +120,20 @@ class AdminApplicationTest {
 		verify(fdao,times(1)).deleteById(5);
 	}
 	
+    @Test
+    public void testgetflightbyid(){
+    	Flight flight=new Flight(5,"XYZ","Emirates",200);
+    	  when(fdao.findById(flight.getFlightNumber())).thenReturn(Optional.of(flight)); 
+		
+     Flight result = flightService.viewFlight(5);
+		assertEquals(5, result.getFlightNumber());
+		assertEquals("XYZ", result.getFlightModel());
+		assertEquals("Emirates", result.getCarrierName());
+		assertEquals(200, result.getSeatCapacity());           
+	}
 
-	@Mock
-	private AirportRepository adao;
-	
-	@InjectMocks
-	private ScheduledflightService airportService;
-	
-	
-	
-	
+
+
 	@Test
 	public void testfindAll() {
 		List<Airport> airportList=new ArrayList<>();
@@ -139,18 +156,63 @@ class AdminApplicationTest {
 	}
 	
 	@Test
-	public void updateAirport() {
-		Airport airport=new Airport("5","Bhegumpet","Hyd");
-		adao.findByairportName("Hyd");
-		adao.save(airport);
-		verify(adao,Mockito.times(1)).save(airport);
+	public void testaddScheduledFlight() {
+		Scheduledflight flight=new Scheduledflight();
+		Flight f=new Flight(1,"Aero","Air India",400);
+		Airport a1=new Airport("Hyd123","Hyderabad","Rajeev gandhi");
+		Airport a2=new Airport("Ban123","Bangaloore","Vor Bangaloore");
+		flight.setScheduledflightid(901);
+		flight.setAvailableSeats(300);
+		flight.setTicketcost(1200);
+		flight.setFlight(f);
+		flight.setSourceairport(a1);
+		flight.setDestinationairport(a2);
+		flight.setDate1("2020-05-11");
+		flight.setArrivaltime(null);
+		flight.setDeparturetime(null);
+		sfdao.save(flight);
+		
+	}
+	
+	
+	
+	@Test
+	public void testfindAllScheduledflight() {
+		List<Scheduledflight> flightList=new ArrayList<>();
+		Flight f=new Flight(1,"Aero","Air India",400);
+		Airport a1=new Airport("Hyd123","Hyderabad","Rajeev gandhi");
+		Airport a2=new Airport("Ban123","Bangaloore","Vor Bangaloore");
+		flightList.add(new Scheduledflight(1,300,1200,f,a1,a2,"2020-05-14",null,null));
+		Mockito.when(sfdao.findAll()).thenReturn(flightList);
+		List<Scheduledflight> result=sfdao.findAll();
+		assertEquals(1,result.size());
 	}
 	
 	@Test
-	public void deleteAirport() {
-		Airport airport=new Airport("6","Ghandi","Shamshabad");
-		adao.deleteById("6");
-		verify(adao,times(1)).deleteById("6");
+	public void deleteScheduledflight() {
+		Flight f=new Flight(1,"Aero","Air India",400);
+		Airport a1=new Airport("Hyd123","Hyderabad","Rajeev gandhi");
+		Airport a2=new Airport("Ban123","Bangaloore","Vor Bangaloore");
+		Scheduledflight sfg= new Scheduledflight(1,300,1200,f,a1,a2,"2020-05-14",null,null);
+		sfdao.deleteById("1");
+		verify(sfdao,times(1)).deleteById("1");
 	}
+	
+	   @Test
+	    public void testgetscheduleflightbyid(){
+			Flight f=new Flight(1,"Aero","Air India",400);
+			Airport a1=new Airport("Hyd123","Hyderabad","Rajeev gandhi");
+			Airport a2=new Airport("Ban123","Bangaloore","Vor Bangaloore");
+			Scheduledflight sfg= new Scheduledflight(1,300,1200,f,a1,a2,"2020-05-14",null,null);
+	    	  when(sfdao.findById(sfg.getScheduledflightid())).thenReturn(Optional.of(sfg)); 
+	     Scheduledflight result = sfService.viewScheduledFlight(1);
+			assertEquals(1,result.getScheduledflightid());
+			assertEquals(300,result.getAvailableSeats());
+			assertEquals(1200,result.getTicketcost());
+			assertEquals(f.getFlightNumber(),result.getFlight().getFlightNumber());
+			assertEquals(a1.getAirportName(),result.getSourceairport().getAirportName());
+			assertEquals(a2.getAirportName(),result.getDestinationairport().getAirportName());
+			assertEquals("2020-05-14",result.getDate1());
+		}
 	
 }
